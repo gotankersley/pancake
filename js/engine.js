21aec8d6e2ef99bed5f0d08e3b6c6bb07a151339
gotankersley <gotankersley@harding.edu>
2016-02-01 17:13:00 -0600

    vlasic dill

--- js/engine.js
+++ js/engine.js
@@ -1,148 +1,141 @@
 'use strict';
 
-//Constants
-var BOX_SIZE = 10;
-var BOX_CENTER = BOX_SIZE/2;
-var IMG_SIZE = 1024;
-var SCENES_PATH = './scenes/0/';
 
-//Global variables
-var container;
-var scene;
-var camera;
-var renderer;
+var Pancake = (function() { //Poor man's namespace (module pattern)
 
-var origin;
-var boxSides;
-var raycaster = new THREE.Raycaster();
-var ID_TO_DIR = {};
 
-//Camera controls
-var mouse = new THREE.Vector2();
-var mouseDown = new THREE.Vector2();
-var looking = false;
-var prevLon;
-var prevLat;
-var lon = 0;//270;
-var lat = 0;
-var phi = 0;
-var theta = 0;
+	//Constants
+	var BOX_SIZE = 10;
+	var BOX_CENTER = BOX_SIZE/2;
+	var IMG_SIZE = 1024;
+	var SCENES_PATH = './scenes/';
+	var DEBUG = false;
+	var ID_TO_DIR = {};
+	var FOV_MAX = 75;2
+	var FOV_MIN = 55;
+	var BUTTON_RIGHT = 3; 
 	
-//Class Engine
-function Engine() {
+	//Module variables
+	var container;
+	var canvas;
+	var scene;
+	var camera;
+	var renderer;
+	
+	var boxSides;
+	var raycaster = new THREE.Raycaster();	
+
+	//Camera controls	
+	var mouseDown = new THREE.Vector2();
+	var looking = false;
+	var prevLon;
+	var prevLat;
+	var lon = 0;//270;
+	var lat = 0;
+	var phi = 0;
+	var theta = 0;
+	
+	//Properties
+	var curScene = 0;	
 		
-	var SCREEN_WIDTH = window.innerWidth;
-	var SCREEN_HEIGHT = window.innerHeight;		
-	var VIEW_ANGLE = 75;
-	var ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT;
-	var NEAR = 0.1;
-	var FAR = 200;	
-
-	//Engine properties
-	this.callbacks = {};
-	this.curScene = 0;
-	
-	
-	//Camera
-	camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
-	camera.target = new THREE.Vector3( 0, 0, 0 );	
-	scene = new THREE.Scene;
-	scene.add(camera);	
-	camera.position.set(0, 0, 0);
-	
-	//Renderer
-	renderer = new THREE.WebGLRenderer( {antialias:true} );		
-	renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);		
-	container = document.createElement( 'container' );
-	document.body.appendChild( container );
-	container.appendChild( renderer.domElement );	
-	renderer.domElement.oncontextmenu = function (e) {
-		e.preventDefault();
-	};	
-	
-	//Events	
-	document.addEventListener( 'mousemove', onMouseMove, false );	
-	document.addEventListener( 'mousedown', onMouseDown, false );	
-	document.addEventListener( 'mouseup', onMouseUp, false );	
-	
-	//Stage
-	setStage();
-	render();
-}
-
-Engine.prototype.addEvents = function(eventName, callback) {
-	this.callbacks[eventName].push(callback);
-}
-
-function setStage() {
-	
-	//Origin
-	//var originGeo = new THREE.CylinderGeometry(0.1, 0.1, 0.1, 10, 1, false);
-	//var originMat = new THREE.MeshNormalMaterial();	
-	//origin = new THREE.Mesh(originGeo, originMat);
-	//scene.add(origin);
-	
-	//Box
+	//Class Engine
+	function Engine() {
+			
+		var SCREEN_WIDTH = window.innerWidth;
+		var SCREEN_HEIGHT = window.innerHeight;		
+		var VIEW_ANGLE = 75;
+		var ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT;
+		var NEAR = 0.1;
+		var FAR = 200;				
 		
-	var DIRS  = ['f', 'l', 'b', 'r', 'd', 'u'];
-	var EXT = '.jpg';
-	
-	var sideGeo = new THREE.PlaneGeometry( BOX_SIZE, BOX_SIZE, BOX_SIZE);
-	var sidePositions = [
-		new THREE.Vector3(0, 0, -BOX_CENTER),
-		new THREE.Vector3(-BOX_CENTER, 0, 0),
-		new THREE.Vector3(0, 0, BOX_CENTER),
-		new THREE.Vector3(BOX_CENTER, 0, -0),		
-		new THREE.Vector3(0, -BOX_CENTER, 0),
-		new THREE.Vector3(0, BOX_CENTER, 0),
-	];
-	var sideRotations = [
-		new THREE.Vector3(0, 0, 0),
-		new THREE.Vector3(0, Math.PI/2, 0),
-		new THREE.Vector3(0, -Math.PI, 0),
-		new THREE.Vector3(0, -Math.PI/2, 0),		
-		new THREE.Vector3(-Math.PI/2, 0, 0),
-		new THREE.Vector3(Math.PI/2, 0, Math.PI),
-	];
-	
-	boxSides = [];
-	for (var i = 0; i < 6; i++) { //Six faces on a cube...
-		var sideMaterial = new THREE.MeshBasicMaterial({		
-			map: THREE.ImageUtils.loadTexture( SCENES_PATH + DIRS[i] + EXT ),		
-		});
-	
-		var side = new THREE.Mesh( sideGeo, sideMaterial );
-		side.position.copy(sidePositions[i]);
-		var euler = new THREE.Euler(sideRotations[i].x, sideRotations[i].y, sideRotations[i].z, 'XYZ');		
-		side.rotation.copy(euler);
-		ID_TO_DIR[side.id] = DIRS[i];		
-		boxSides.push(side);
-		scene.add(side);
+		//Camera
+		camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+		camera.target = new THREE.Vector3( 0, 0, 0 );	
+		scene = new THREE.Scene;
+		scene.add(camera);	
+		camera.position.set(0, 0, 0);
+		
+		//Renderer
+		renderer = new THREE.WebGLRenderer( {antialias:true} );		
+		renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);		
+		container = document.createElement( 'container' );
+		document.body.appendChild( container );
+		container.appendChild( renderer.domElement );	
+		canvas = renderer.domElement;
+		canvas.oncontextmenu = function (e) {
+			e.preventDefault();
+		};	
+		
+		//Events	
+		document.addEventListener( 'mousemove', onMouseMove, false );	
+		document.addEventListener( 'mousedown', onMouseDown, false );	
+		document.addEventListener( 'mouseup', onMouseUp, false );	
+		document.addEventListener( 'mousewheel', onMouseWheel, false );
+		document.addEventListener( 'MozMousePixelScroll', onMouseWheel, false);
+		window.addEventListener( 'resize', onWindowResize, false );
+		
+		//Stage
+		setStage();
+		render();
 	}
-		
-}
 
-function onMouseMove( e ) {
-
-	if (looking) {
-		lon = ( mouseDown.x - e.clientX ) * 0.1 + prevLon;
-		lat = ( e.clientY - mouseDown.y ) * 0.1 + prevLat;
+	//Public functions
+	Engine.prototype.addEvents = function(eventName, callback) {
+		callbacks[eventName].push(callback);
 	}
-	mouse.x = (e.clientX  / window.innerWidth ) * 2 - 1;	
-	mouse.y = - (e.clientY / window.innerHeight ) * 2 + 1;	
-	picking(mouse, false);	
 
-}
-
-function picking(mouse, click) {
-	// update the picking ray with the camera and mouse position	
-	raycaster.setFromCamera( mouse, camera );	
-
-	// calculate objects intersecting the picking ray
-	var intersects = raycaster.intersectObjects( boxSides);	
-	if ( intersects.length > 0 ) {
+	//Private
+	function setStage() {
+			
+		//Box			
+		var DIRS  = ['f', 'l', 'b', 'r', 'd', 'u'];
+		var EXT = '.jpg';
 		
-		var col = intersects[0];
+		var sideGeo = new THREE.PlaneGeometry( BOX_SIZE, BOX_SIZE, BOX_SIZE);
+		var sidePositions = [
+			new THREE.Vector3(0, 0, -BOX_CENTER),
+			new THREE.Vector3(-BOX_CENTER, 0, 0),
+			new THREE.Vector3(0, 0, BOX_CENTER),
+			new THREE.Vector3(BOX_CENTER, 0, -0),		
+			new THREE.Vector3(0, -BOX_CENTER, 0),
+			new THREE.Vector3(0, BOX_CENTER, 0),
+		];
+		var sideRotations = [
+			new THREE.Vector3(0, 0, 0),
+			new THREE.Vector3(0, Math.PI/2, 0),
+			new THREE.Vector3(0, -Math.PI, 0),
+			new THREE.Vector3(0, -Math.PI/2, 0),		
+			new THREE.Vector3(-Math.PI/2, 0, 0),
+			new THREE.Vector3(Math.PI/2, 0, Math.PI),
+		];
+		
+		boxSides = [];
+		for (var i = 0; i < 6; i++) { //Six faces on a cube...
+			var sideMaterial = new THREE.MeshBasicMaterial({		
+				map: THREE.ImageUtils.loadTexture( SCENES_PATH + curScene + '/' + DIRS[i] + EXT ),		
+			});
+		
+			var side = new THREE.Mesh( sideGeo, sideMaterial );
+			side.position.copy(sidePositions[i]);
+			var euler = new THREE.Euler(sideRotations[i].x, sideRotations[i].y, sideRotations[i].z, 'XYZ');		
+			side.rotation.copy(euler);
+			ID_TO_DIR[side.id] = DIRS[i]; //Store object's id for easy converting of id to box side	
+			boxSides.push(side);
+			scene.add(side);
+		}
+			
+	}
+
+
+	function getCollisions(mouse) {
+		// update the picking ray with the camera and mouse position	
+		raycaster.setFromCamera( mouse, camera );	
+
+		// calculate objects intersecting the picking ray
+		return raycaster.intersectObjects( boxSides);	
+	}
+	
+	function collisionPointToTexturePoint(col) { //Convert point in 3D space to texture x,y for hit testing
 		var point3d = col.point; //Point in 3D space
 		var dir = ID_TO_DIR[col.object.id];
 		var point2d = new THREE.Vector2();
@@ -175,90 +168,126 @@ function picking(mouse, click) {
 		
 			
 		//cross multiply to get in the range of [0-1023]
-		var point = [
+		return [
 			(IMG_SIZE * point2d.x)/BOX_SIZE, //x
 			(IMG_SIZE * point2d.y)/BOX_SIZE  //y
 		];
-		
-		var regions = nav[0];	
-		var found = false;
-		for (var r = 0; r < regions.length; r++) {
-			var region = regions[r];
-			if (inside(point, region.coords)) {
-				//this.curRegion = region;
-				renderer.domElement.style.cursor = 'pointer';
-				//this.curCursor = region.cursor;
-				//console.log(region.cursor);
-				found = true;
-				if (click) {
-					SCENES_PATH = 'scenes/6/';
-					scene.remove(boxSides);
-					setStage();
+	}
+	
+	function getActiveRegion(mouse) {
+		var collisions = getCollisions(mouse);
+		if ( collisions.length > 0 ) {
+			
+			var col = collisions[0];
+			var point2d = collisionPointToTexturePoint(col);
+						
+			var regions = nav[curScene];	
+			
+			for (var r = 0; r < regions.length; r++) {
+				var region = regions[r];
+				if (inside(point2d, region.coords)) {
+					if (DEBUG) console.log(dir, point2d);
+					return region;										
 				}
-				break;
 			}
+			
+			
 		}
-		if (!found) renderer.domElement.style.cursor = 'default';
-		//console.log(dir, point);
-	}		
-}
+		return null;
+	}
 
-function onMouseDown( e ) {
-	e.preventDefault();
-	looking = true;
-	mouseDown.x = e.clientX;
-	mouseDown.y = e.clientY;
-	
-	prevLon = lon;
-	prevLat = lat;
-	
-	mouse.x = (e.clientX  / window.innerWidth ) * 2 - 1;	
-	mouse.y = - (e.clientY / window.innerHeight ) * 2 + 1;	
-	picking(mouse, true);
-}
+	function onMouseMove( e ) {
 
-function onMouseUp( e ) {
-	looking = false;
-}
-
-//Collision detection
-function inside (point, vs) { //https://github.com/substack/point-in-polygon
-    // ray-casting algorithm based on
-    // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
-    
-    var x = point[0], y = point[1];
-    
-    var inside = false;
-    for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
-        var xi = vs[i][0], yi = vs[i][1];
-        var xj = vs[j][0], yj = vs[j][1];
-        
-        var intersect = ((yi > y) != (yj > y))
-            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
-        if (intersect) inside = !inside;
-    }
-    
-    return inside;
-};
-
-function render(time) {	
-	
-	
-	
-	lat = Math.max( - 85, Math.min( 85, lat ) );
-	phi = THREE.Math.degToRad( 90 - lat );
-	theta = THREE.Math.degToRad( lon );
-	
-	camera.target.x = 500 * Math.sin( phi ) * Math.cos( theta );
-	camera.target.y = 500 * Math.cos( phi );
-	camera.target.z = 500 * Math.sin( phi ) * Math.sin( theta );
-
-	camera.lookAt( camera.target );
+		if (looking) {
+			lon = ( mouseDown.x - e.clientX ) * 0.1 + prevLon;
+			lat = ( e.clientY - mouseDown.y ) * 0.1 + prevLat;
+		}
+		var mouse = new THREE.Vector2();
+		mouse.x = (e.clientX  / window.innerWidth ) * 2 - 1;	
+		mouse.y = - (e.clientY / window.innerHeight ) * 2 + 1;
 		
-	
-	renderer.render( scene, camera );
-	requestAnimationFrame( render );	
-}
-//End class Engine
+		var region = getActiveRegion(mouse);
+		if (region) {
+			if (region.cursor == 'f') canvas.style.cursor = 'pointer';
+			else if (region.cursor == 'm') canvas.style.cursor = 'zoom-in';
+		}
+		else canvas.style.cursor = 'default';		
 
-var engine = new Engine();
+	}
+	
+	function onMouseDown( e ) {
+		e.preventDefault();				
+		
+		if (e.which == BUTTON_RIGHT) { //Hotspot / navigation 
+			var mouse = new THREE.Vector2();
+			mouse.x = (e.clientX  / window.innerWidth ) * 2 - 1;	
+			mouse.y = - (e.clientY / window.innerHeight ) * 2 + 1;	
+			var region = getActiveRegion(mouse);
+			if (region) onNavigationEvent(region);
+				
+		}
+		else { //Camera looking
+			looking = true;
+			mouseDown.x = e.clientX;
+			mouseDown.y = e.clientY;
+		
+			prevLon = lon; //Store previous so we can get the drag delta
+			prevLat = lat;
+		}
+		
+	}
+
+	function onMouseUp( e ) {
+		looking = false;
+	}
+
+	function onMouseWheel( e ) {
+		
+		if ( e.wheelDeltaY ) camera.fov -= e.wheelDeltaY * 0.05; // WebKit
+		else if ( e.wheelDelta ) camera.fov -= e.wheelDelta * 0.05;  // Opera / Explorer 9
+		else if ( e.detail ) camera.fov += e.detail * 1.0; // Firefox
+
+		camera.fov = Math.max(FOV_MIN, camera.fov);
+		camera.fov = Math.min(FOV_MAX, camera.fov);
+		camera.updateProjectionMatrix();
+
+	}
+
+	function onWindowResize() {
+
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+
+		renderer.setSize( window.innerWidth, window.innerHeight );
+
+	}
+	
+	function onNavigationEvent(region) {
+		scene.remove(boxSides);
+		curScene = region.target;
+		//setStage();
+	}
+	
+	function render(time) {	
+		
+
+		lat = Math.max( - 85, Math.min( 85, lat ) );
+		phi = THREE.Math.degToRad( 90 - lat );
+		theta = THREE.Math.degToRad( lon );
+		
+		camera.target.x = 500 * Math.sin( phi ) * Math.cos( theta );
+		camera.target.y = 500 * Math.cos( phi );
+		camera.target.z = 500 * Math.sin( phi ) * Math.sin( theta );
+
+		camera.lookAt( camera.target );
+					
+		renderer.render( scene, camera );
+		requestAnimationFrame( render );	
+	}
+	//End class Engine
+
+	//Exports
+	return {Engine:Engine};
+
+})(); //End Pancake namespace
+var pan = new Pancake.Engine();
